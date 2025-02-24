b148ae3c209849d6dd29cb52fcde10b9e25e5347
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-02-24 10:39:19 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,45 @@
 radio.setGroup(73)
-
 enum State { Ready, Running, Finnish }
-let akce = State.Ready
-Sensors.SetLightLevel()
+let lastTime: number;
+let akce: State;
+const reset = () => {
+    lastTime = 0
+    akce = State.Ready
+    Sensors.SetLightLevel()
+}
+reset()
 
 Sensors.OnLightDrop(function () {
     if (akce === State.Ready) {
-        radio.sendNumber(1) // konec = 2, start = 1
+        radio.sendNumber(1) // start
         akce = State.Running
     }
-
 })
-
-//radio.onReceivedNumber(function(receivedNumber: number) {
-// whaleysans.showNumber(input.runningTime())
-//})
-
 
 radio.onReceivedValue(function (name: string, value: number) {
     music.playTone(440, 500)
 
     if (name === "endTime" && akce === State.Running) {
-        basic.showNumber(value)
+        lastTime = value
+        basic.showNumber(lastTime)
         music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         akce = State.Finnish
     }
-
+})
+input.onButtonPressed(Button.A, function() {
     if (akce === State.Finnish) {
-        akce = State.Ready
-    }
+   basic.showNumber(lastTime)
+}
 })
 
+input.onButtonPressed(Button.B, function() {
+    radio.sendNumber(2)
+    reset();
+})
 
-
+radio.onReceivedNumber(function (receivedNumber: number) {
+    if (receivedNumber === 0) {
+        //radio.sendNumber(0)
+        reset();
+    }
+})
